f5f8bee8225eea730ce0e8b97f0a5d579cb0334f
Linda Njau <lindanjau21@gmail.com>
2024-03-21 15:00:46 +0300

    Change to file structure

--- COOKOUT/backend/app/static/index.js
+++
@@ -1,8 +0,0 @@
-functin deleteRecipe(recipeId) {
-    fetch("/delete-recipe", {
-        method: "POST",
-        body: JSON.stringify({ recipeId: recipeId }),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
